d9058cd0b22cdcaf62c2b2cd540d452f63c72f1d
valeriaparra123 <101677456+valeriaparra123@users.noreply.github.com>
2023-02-09 16:21:42 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 img = "";
-
+status = "";
 function preload()
 {
  img = loadImage('dog_cat.jpg');
@@ -9,6 +9,8 @@ function setup()
 {
   canvas  = createCanvas(640, 420);
   canvas.center();
+  objectDectector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Estatus: dectectando objetos";
 }
 
 function draw() 
@@ -19,4 +21,24 @@ function draw()
     noFill();
     stroke("#FF00000");
     rect(30, 60, 450, 350 );
+
+    fill("FF0000");
+    text("Gato", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320 );
+}
+
+function modelLoaded()
+{
+  console.log("¡modelo cargado!");
+  status=true;
+  objectDectector.detect(img, gotResult);
+}
+function gotResult() 
+{
+  if (error) {
+ console.log(error);   
+  }
+  console.log(results);
 }
